eda04673574b3a40d259af1d91b65ab9d83e7c81
Talluri Himabindu <tallurihimabindu342@gmail.com>
2026-08-12 05:02:24 +0000

    Added exec system call program

---
+++ 2520030484_Practical/Week 3-2 Practical/exec.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Before exec()\n");
+
+    execl("/bin/ls", "ls", "-l", NULL);
+
+    printf("After exec()\n");
+
+    return 0;
+}
